1f0da1f5a89e7fd076bfbe7aa5d186504cc394a2
vaiperlexmar <vaiperlexmar@gmail.com>
2023-06-26 13:30:11 +0300

    made a hamburger menu

--- script/main.js
+++ script/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const navLinks = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("#nav");
+
+function toggleMenu() {
+  if (nav.classList.contains("open")) {
+    nav.classList.remove("appearance-animation");
+    nav.classList.add("disappearance-animation");
+    setTimeout(function () {
+      nav.classList.remove("open");
+    }, 400);
+  } else {
+    nav.classList.remove("disappearance-animation");
+    nav.classList.add("open");
+
+    nav.classList.add("appearance-animation");
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
